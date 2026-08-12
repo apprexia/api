937bf41d41b4c7b1c2db28cc9be83d655eb63f92
dphengsiaroun <dphengsiaroun@outlook.fr>
2026-08-12 20:00:26 +0200

    change headless to false

--- src/services/meta-data-scrapper/meta-data-scrapper.service.ts
+++ src/services/meta-data-scrapper/meta-data-scrapper.service.ts
@@ -653,12 +653,8 @@ export class MetadataScraperService implements OnModuleInit, OnModuleDestroy {
             this.logger.log('🚀 Launch Chromium');
 
             this.browser = await chromium.launch({
-                headless: true,
-                args: [
-                    '--no-sandbox',
-                    '--disable-setuid-sandbox',
-                    '--disable-blink-features=AutomationControlled',
-                ],
+                headless: false,
+                args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-blink-features=AutomationControlled'],
             });
         }
 
